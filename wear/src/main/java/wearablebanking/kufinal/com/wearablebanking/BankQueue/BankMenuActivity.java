f1bf5dfdde25d27aabfd38aae70ae36d99a7f949
onur keskin <onurkeskin@ku.edu.tr>
2015-11-13 02:38:06 +0200

    Over Finalize

--- wear/src/main/java/wearablebanking/kufinal/com/wearablebanking/BankQueue/BankMenuActivity.java
+++ wear/src/main/java/wearablebanking/kufinal/com/wearablebanking/BankQueue/BankMenuActivity.java
@@ -1,11 +1,16 @@
 package wearablebanking.kufinal.com.wearablebanking.BankQueue;
 
+import android.animation.TimeAnimator;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.Button;
@@ -18,9 +23,16 @@ import java.util.Date;
 import wearablebanking.kufinal.com.wearablebanking.Location.GeneralLocationRequestService;
 import wearablebanking.kufinal.com.wearablebanking.Location.LocationInfoService;
 import wearablebanking.kufinal.com.wearablebanking.R;
+import com.google.gson.Gson;
+
 
 public class BankMenuActivity extends Activity implements BankMenuFragment.BankFragmentListener{
 
+    private final String queue_timer_prefs = "QueueTimerPrefs";
+    private final String queue_timer_pref_name = "QueueTimerPrefsName";
+
+    private String timeRemainingText;
+    private int queue;
 
     private Button Bank_info_btn;
     private Button get_queue_btn;
@@ -50,6 +62,16 @@ public class BankMenuActivity extends Activity implements BankMenuFragment.BankF
         Intent intent = getIntent();
         double[] coors = intent.getDoubleArrayExtra("coordinates");
         req = LocationInfoService.getInstance().parseLocation(coors);
+
+        SharedPreferences settings = getSharedPreferences(queue_timer_prefs, 0);
+        String queueTimer = settings.getString(queue_timer_pref_name, "");
+
+        if(!queueTimer.equals("")){
+            Gson gson = new Gson();
+            QueueOccupationInfo queue = gson.fromJson(queueTimer, QueueOccupationInfo.class);
+            arrivalSetTimer(queue);
+        }
+
     }
 
 
@@ -60,17 +82,76 @@ public class BankMenuActivity extends Activity implements BankMenuFragment.BankF
 
         QueueOccupationInfo arrival = req.requestQueue(c.getTime());
 
-        String time = arrival.calculateRemainingTime();
-        int queue = arrival.getQueueNum();
-
-        String[] times = time.split(" ");
-        String totalTime = times[0]+ ":" + times[1]+ ":" + times[2]+ ":" + times[3];
-        get_queue_btn.setText("Your Queue Number: " + queue + "\nExpected Wait Time = " + totalTime);
-        get_queue_btn.setEnabled(false);
+        arrivalSetTimer(arrival);
     }
 
     @Override
     public void getInfoClicked() {
 
+    }
+
+    private void arrivalSetTimer(QueueOccupationInfo arrival){
+        long time = arrival.calculateRemainingTime();
+        queue = arrival.getQueueNum();
+
+        long diffSeconds = time / 1000 % 60;
+        long diffMinutes = time / (60 * 1000) % 60;
+        long diffHours = time / (60 * 60 * 1000) % 24;
+        long diffDays = time / (24 * 60 * 60 * 1000);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(arrival);
+
+        SharedPreferences settings = getSharedPreferences(queue_timer_prefs, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(queue_timer_pref_name, json);
+        editor.commit();
+
+        timeRemainingText = diffDays + ":"
+                + diffHours + ":"
+                + diffMinutes + ":"
+                + diffSeconds;
+
+        final Handler timerHandler = new Handler(){
+            public void handleMessage(Message msg) {
+                get_queue_btn.setText("Your Queue Number: " + queue +
+                        "\nExpected Wait Time = " + timeRemainingText);
+            }
+        };
+
+        get_queue_btn.setText("Your Queue Number: " + queue +
+                "\nExpected Wait Time = " + timeRemainingText);
+
+        new CountDownTimer(time, 1000){
+            public void onTick(long millisUntilFinished) {
+                long diffSeconds = millisUntilFinished / 1000 % 60;
+                long diffMinutes = millisUntilFinished / (60 * 1000) % 60;
+                long diffHours = millisUntilFinished / (60 * 60 * 1000) % 24;
+                long diffDays = millisUntilFinished / (24 * 60 * 60 * 1000);
+                String text = "";
+
+                if(diffDays > 0){
+                    text += diffDays + ":";
+                }
+                if(diffHours > 0){
+                    text += diffHours + ":";
+                }
+                if(diffMinutes > 0){
+                    text += diffMinutes + ":";
+                }
+                if(diffSeconds > 0){
+                    text += diffSeconds;
+                }
+
+                timeRemainingText = text;
+                timerHandler.dispatchMessage(new Message());
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        get_queue_btn.setEnabled(false);
     }
 }
